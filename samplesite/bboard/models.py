bd5432183eecd1a94fddcb80ce6b1d57210a5671
darkwolf <darkwolf1990janary@gmail.com>
2023-10-15 20:47:09 +0300

    create class AdvUSer

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
@@ -26,3 +28,8 @@ class Rubric(models.Model):
 
 class Meta:
     verbose_name_plural = 'Рубрики'
+
+
+class AdvUser(models.Model):
+    is_activated = models.BooleanField(default=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
